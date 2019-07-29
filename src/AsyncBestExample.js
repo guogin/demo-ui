f7a1c08b477acaafa88172fa1e499d57993013b0
Guo, Yong Kang <guogin@outlook.com>
2019-07-29 18:55:18 +0800

    Remove answers

--- src/AsyncBestExample.js
+++ src/AsyncBestExample.js
@@ -5,25 +5,7 @@ function asyncBestExample(e) {
 
     resetPageContent();
 
-    getJSON('/api/story.json').then(function (story) {
-        addHtmlToPage(story.heading);
-        // Map our array of chapter urls
-        // to an array of chapter json promises
-        return story.chapterUrls.map(getJSON).reduce(function(chain, chapterPromise) {
-            // Use reduce to chain the promises together,
-            // but adding content to the page for each chapter
-            return chain.then(function() {
-                return chapterPromise;
-            }).then(function(chapter) {
-                addHtmlToPage(chapter.html);
-            });
-        }, Promise.resolve());
-    }).then(function() {
-        addTextToPage("All done");
-    }).catch(function(err) {
-        // catch any error that happened along the way
-        addTextToPage("Argh, broken: " + err.message);
-    });
+    //TODO: Implementation goes below
 
 }
 
